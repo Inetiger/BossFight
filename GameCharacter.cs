d8c7e594cc4abb0027e584400999cde9d4057eb7
Inetiger <ivensen18@gmail.com>
2023-12-15 13:50:54 +0100

    Add project files.

---
+++ GameCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BossFight
+{
+    internal class GameCharacter
+    {
+        Random random = new Random();
+
+        public int Health;
+        public int Strength;
+        public int Stamina;
+        private int _originStamina;
+
+        public GameCharacter(int health, int strength, int stamina)
+        {
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            _originStamina = stamina;
+        }
+
+        public void Fight(GameCharacter Hero, GameCharacter Boss)
+        {
+            if (Strength != 20)
+            {
+                int bossStrength = random.Next(0, Strength);
+                Hero.Health -= bossStrength;
+                Console.WriteLine($"Enemy Hit Hero with {bossStrength} damage, Hero now has {Hero.Health} health left.");
+                Boss.Stamina -= 10;
+            }
+            else
+            {
+                Boss.Health -= Strength;
+                Console.WriteLine($"Hero hit Enemy with {Strength} damage, Enemy now has {Boss.Health} health left.");
+                Hero.Stamina -= 10;
+            }
+        }
+
+        public void Recharge(string current)
+        {
+            Stamina = _originStamina;
+            Console.WriteLine($"{current} recharged back up to {_originStamina} stamina");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace BossFight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        private static void Start()
+        {
+            GameCharacter Hero = new GameCharacter(100, 20, 40);
+            GameCharacter Boss = new GameCharacter(400, 30, 10);
+
+            while (Boss.Health >= 0 && Hero.Health >= 0)
+            {
+                HeroTurn();
+                BossTurn();
+            }
+            if (Hero.Health <= 0)
+            {
+                switch (Ask("Hero Lost\nDo you want to try again? y/n"))
+                {
+                    case "y":
+                        Start();
+                        break;
+                    case "n":
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+            else
+            {
+                switch (Ask("Hero Won\nDo you want to try again? y/n"))
+                {
+                    case "y":
+                        Start();
+                        break;
+                    case "n":
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+
+            void HeroTurn()
+            {
+                switch (AskInt("Hero's Turn:\n1. Fight\n2. Recharge"))
+                {
+                    case 1:
+                        if (Hero.Stamina >= 10) Hero.Fight(Hero, Boss);
+                        else
+                        {
+                            Console.WriteLine("Sorry, not enough stamina.");
+                            HeroTurn();
+                        }
+                        break;
+                    case 2:
+                        Hero.Recharge("Hero");
+                        break;
+                }
+            }
+
+            void BossTurn()
+            {
+                Console.WriteLine("Boss's Turn:");
+                if (Boss.Stamina >= 10) Boss.Fight(Hero, Boss);
+                else Boss.Recharge("Enemy");
+            }
+        }
+
+        private static string Ask(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        private static int AskInt(string question)
+        {
+            return Convert.ToInt32(Ask(question));
+        }
+    }
+}
